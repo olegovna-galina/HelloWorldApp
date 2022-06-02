f22194806f72d57f1029ce3fbe15fb7e2b260460
olegovna-galina <galga_1@tut.by>
2022-06-02 12:56:47 +0300

    test

---
+++ src/com/rest/Request.java
@@ -0,0 +1,23 @@
+package com.rest;
+import java.util.Date;
+
+public class Request {
+    Car car;
+    Date datePurchased;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(final Car car) {
+        this.car = car;
+    }
+
+    public Date getDatePurchased() {
+        return datePurchased;
+    }
+
+    public void setDatePurchased(final Date datePurchased) {
+        this.datePurchased = datePurchased;
+    }
+}
